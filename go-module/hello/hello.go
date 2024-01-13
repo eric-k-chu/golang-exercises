9a2d4c0dbfbfff525b2450b6d751b13b806bfa98
eric-k-chu <kiyuen88ec@gmail.com>
2024-01-13 13:52:23 -0800

    completed go-module

--- go-module/hello/hello.go
+++ go-module/hello/hello.go
@@ -2,11 +2,22 @@ package main
 
 import (
 	"fmt"
+	"log"
 
 	"golang-exercises/greetings"
 )
 
 func main() {
-	message := greetings.Hello("Eric")
-	fmt.Println(message)
+	log.SetPrefix("greetings: ")
+	log.SetFlags(0)
+
+	names := []string{"Eric", "Erik", "Erick"}
+
+	messages, err := greetings.Hellos(names)
+
+	if err != nil {
+		log.Fatal(err)
+	}
+
+	fmt.Println(messages)
 }
