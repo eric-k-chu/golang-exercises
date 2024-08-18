f156c198aaac78127a807cdaf70ce2c0e8f08f28
eric-k-chu <kiyuen88ec@gmail.com>
2024-08-18 16:48:33 -0700

    added struct tags

--- user/types.go
+++ user/types.go
@@ -1,31 +1,31 @@
 package user
 
 type User struct {
-	ID       int16
-	Name     string
-	Username string
-	Email    string
-	Address  Address
-	Phone    string
-	Website  string
-	Company  TinyCompany
+	ID       int16       `json:"id"`
+	Name     string      `json:"name"`
+	Username string      `json:"username"`
+	Email    string      `json:"email"`
+	Address  Address     `json:"address"`
+	Phone    string      `json:"phone"`
+	Website  string      `json:"website"`
+	Company  TinyCompany `json:"company"`
 }
 
 type Address struct {
-	Street  string
-	Suite   string
-	City    string
-	Zipcode string
-	Geo     LatLng
+	Street  string `json:"street"`
+	Suite   string `json:"suite"`
+	City    string `json:"city"`
+	Zipcode string `json:"zipcode"`
+	Geo     LatLng `json:"geo"`
 }
 
 type LatLng struct {
-	Lat string
-	Lng string
+	Lat string `json:"lat"`
+	Lng string `json:"lng"`
 }
 
 type TinyCompany struct {
-	Name        string
-	CatchPhrase string
-	BS          string
+	Name        string `json:"name"`
+	CatchPhrase string `json:"catchPhrase"`
+	BS          string `json:"bs"`
 }
